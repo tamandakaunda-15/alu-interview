0494300b6cf40c00930698ae4bd01bc82fcef377
tamandakaunda-15 <tamandakaunda900@gmail.com>
2024-05-26 15:53:32 +0000

    alu interview

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,28 +4,35 @@
 
 
 def minOperations(n):
-     """Calculate the fewest number of operations needed to result in exactly n H characters in the file."""
+    """Calculates the fewest number of operations needed to result in
+    exactly n H characters in the file.
 
+    In a text file, there is a single character H. Your text editor can
+    execute only two operations in this file: Copy All and Paste characters
+    in the file.
 
- if n < 2:
+    Args:
+        n (int): The number of desired H characters.
+
+    Returns:
+        int: The number of minimal operations needed to get n H characters
+    or 0 if it is impossible to achieve n.
+    """
+    if not isinstance(n, int):
         return 0
-
-# If n is less than 2 ,the operation will not work
-
- operations = 0
-    divisor = 2
-
-while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
-
-    return operations
-
-
-# Here's an example 
-
-print(minOperations(9))
-
-# the example above prints 6
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count

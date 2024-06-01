b13154a9e6d8120d98adb78e16f2e879c9d79953
tamandakaunda-15 <tamandakaunda900@gmail.com>
2024-06-01 16:02:32 +0000

    calculate the amount of water retained

--- 0-rain.py/0-rain.py
+++ 0-rain.py/0-rain.py
@@ -8,16 +8,12 @@ def rain(walls):
 
     while left < right:
         if walls[left] < walls[right]:
-            if walls[left] >= left_max:
-                left_max = walls[left]
-            else:
-                water += left_max - walls[left]
+            left_max = max(left_max, walls[left])
+            water += left_max - walls[left]
             left += 1
         else:
-            if walls[right] >= right_max:
-                right_max = walls[right]
-            else:
-                water += right_max - walls[right]
+            right_max = max(right_max, walls[right])
+            water += right_max - walls[right]
             right -= 1
 
     return water

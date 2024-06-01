51b6dfed5e6af231ec7a179c5c2a34d5196753df
tamandakaunda-15 <tamandakaunda900@gmail.com>
2024-06-01 15:56:39 +0000

    solving this problem using python

---
+++ 0-rain.py/0-rain.py
@@ -0,0 +1,23 @@
+def rain(walls):
+    if not walls:
+        return 0
+
+    left, right = 0, len(walls) - 1
+    left_max, right_max = 0, 0
+    water = 0
+
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
+
+    return water
